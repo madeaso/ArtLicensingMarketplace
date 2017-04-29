26194c25f58e0fc457e312ad1cfb6228040d4dde
Melanie <mlhgst>
2017-04-28 21:39:47 -0700

    initial commit

---
+++ react-bootstrap-boilerplate-master/build/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ModalInstance from './BootstrapComponent';
+
+
+ReactDOM.render(ModalInstance, document.getElementById('content'));
---
+++ react-bootstrap-boilerplate-master/webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+
+  module.exports = {
+    context: process.cwd(),
+    entry: {
+      script: './build/App.jsx'
+  },
+    output: {
+      path: './public',
+      filename: "[name].js",
+      chunkFilename: "[id].js"
+    },
+
+    devtools: 'eval-source-map',
+
+    module: {
+      loaders: [
+        { test: /\.css$/,
+          loader: 'style-loader!css-loader' },
+        { test: /\.jsx$/, exclude: (/node_modules/), loader: 'babel-loader',
+          query: {
+            presets: ['es2015', 'react']
+          }
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['', '.js', '.json', '.jsx']
+    }
+  };
