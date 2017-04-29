dda9da190bdb3d72f556228316747c44eec31f7a
Melanie <mlhgst>
2017-04-29 14:34:02 -0700

    removed old boilerplate

--- react-bootstrap-boilerplate-master/build/App.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ModalInstance from './BootstrapComponent';
-
-
-ReactDOM.render(ModalInstance, document.getElementById('content'));
