4536a531768da1171d8b959b33528fb1cd22b134
Melanie <mhgiusti@cpp.edu>
2017-06-03 11:36:53 -0700

    connection to mongodb established from client frontend (yay :] !)

--- react-bootstrap-boilerplate/build/Header/SubmitModal.jsx
+++ react-bootstrap-boilerplate/build/Header/SubmitModal.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Modal, Button} from 'react-bootstrap';
+import {Modal, Button,DropdownButton,MenuItem} from 'react-bootstrap';
 import ImagesUploader from 'react-images-uploader';
+import axios from 'axios';
 
 
 import 'react-images-uploader/styles.css';
@@ -14,16 +15,48 @@ class PurchaseModal extends React.Component {
     constructor(props){
         super(props);
         this.state={
+            artist:'',
             title:'',
             price:'',
             desc:'',
-            img:''
+            img:'',
+            dropDownTitle:'Subject',
+            dropDownItems:['Photography','Illustration','Graphic Desgin','8 Bit Digital','Game Concept','Oil','Watercolor','Concept','Pastel','Digital Illustration','3D','Comics','Mixed Media','Anaglyph'],
         }
+        this.state.dropDownItems.sort();
     }
 
-    submit(e,title,price,desc,img){
-    
-        this.setState({title:title,price:price,desc:desc,img:img});
+    dropDownSelect(selectedItem,index){
+        var items = this.state.dropDownItems;
+        var replace;
+
+        if(this.state.dropDownTitle == 'Subject'){
+            replace = 'None';
+        }else{
+            replace = this.state.dropDownTitle;
+        }
+        items[index] = replace;
+
+        this.state.dropDownItems = this.state.dropDownItems.sort();
+        this.setState({dropDownTitle:selectedItem,dropDownItems:items});
+    }
+
+    submit(e,artist,title,price,desc,img){
+        axios.post("http://localhost:9090/insert",{
+            artist:artist,
+            title:title,
+            price:price,
+            desc:desc,
+            img:img
+        })
+        .then(function (response){
+            console.log(response);
+        })
+        .catch(function (error) {
+                console.log(error);
+        });
+
+        this.setState({artist:artist,title:title,price:price,desc:desc,img:img});
     }
 
     render(){
@@ -49,7 +82,24 @@ class PurchaseModal extends React.Component {
                             <input id="submit-field-title"  type="text" placeholder="  Title"/>
                             <input id="submit-field-price" type="text" placeholder="  Price"/>
                             <textarea id="submit-field-textarea" maxlength="150" placeholder="  Description"></textarea>
-                            <Button id="submit-button" onClick={this.submit.bind(this,'title,price,desc,img')}>Submit</Button>
+                            <DropdownButton title={this.state.dropDownTitle} id="dropdown-submit">
+                                <MenuItem eventKey="1" onSelect={this.dropDownSelect.bind(this,this.state.dropDownItems[0],0)}>{this.state.dropDownItems[0]}</MenuItem>
+                                <MenuItem eventKey="2" onSelect={this.dropDownSelect.bind(this,this.state.dropDownItems[1],1)}>{this.state.dropDownItems[1]}</MenuItem>
+                                <MenuItem eventKey="3" onSelect={this.dropDownSelect.bind(this,this.state.dropDownItems[2],2)}>{this.state.dropDownItems[2]}</MenuItem>
+                                <MenuItem eventKey="1" onSelect={this.dropDownSelect.bind(this,this.state.dropDownItems[3],3)}>{this.state.dropDownItems[3]}</MenuItem>
+                                <MenuItem eventKey="2" onSelect={this.dropDownSelect.bind(this,this.state.dropDownItems[4],4)}>{this.state.dropDownItems[4]}</MenuItem>
+                                <MenuItem eventKey="3" onSelect={this.dropDownSelect.bind(this,this.state.dropDownItems[5],5)}>{this.state.dropDownItems[5]}</MenuItem>
+                                <MenuItem eventKey="1" onSelect={this.dropDownSelect.bind(this,this.state.dropDownItems[6],6)}>{this.state.dropDownItems[6]}</MenuItem>
+                                <MenuItem eventKey="2" onSelect={this.dropDownSelect.bind(this,this.state.dropDownItems[7],7)}>{this.state.dropDownItems[7]}</MenuItem>
+                                <MenuItem eventKey="3" onSelect={this.dropDownSelect.bind(this,this.state.dropDownItems[8],8)}>{this.state.dropDownItems[8]}</MenuItem>
+                                <MenuItem eventKey="2" onSelect={this.dropDownSelect.bind(this,this.state.dropDownItems[9],9)}>{this.state.dropDownItems[9]}</MenuItem>
+                                <MenuItem eventKey="3" onSelect={this.dropDownSelect.bind(this,this.state.dropDownItems[10],10)}>{this.state.dropDownItems[10]}</MenuItem>
+                                <MenuItem eventKey="1" onSelect={this.dropDownSelect.bind(this,this.state.dropDownItems[11],11)}>{this.state.dropDownItems[11]}</MenuItem>
+                                <MenuItem eventKey="2" onSelect={this.dropDownSelect.bind(this,this.state.dropDownItems[12],12)}>{this.state.dropDownItems[12]}</MenuItem>
+                                <MenuItem eventKey="3" onSelect={this.dropDownSelect.bind(this,this.state.dropDownItems[13],13)}>{this.state.dropDownItems[13]}</MenuItem>
+                                <MenuItem eventKey="1" onSelect={this.dropDownSelect.bind(this,this.state.dropDownItems[14],14)}>{this.state.dropDownItems[14]}</MenuItem>
+                            </DropdownButton>
+                            <Button id="submit-button" onClick={this.submit.bind(this,'Kian','11','$130.00','lorem ipsum valore','imageurl')}>Submit</Button>
                         </div>
                     </div>
                 </Modal.Body>
--- react-bootstrap-boilerplate/server.jsx
+++ react-bootstrap-boilerplate/server.jsx
@@ -4,6 +4,14 @@ import imagesUpload from 'images-upload-middleware';
 
 const app = express();
 
+var MongoClient = require('mongodb');
+var assert = require('assert');
+var url = 'mongodb://localhost:27017/blockart';
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
 app.use('/static',express.static('./server/static'));
 
 app.use(corsPrefetch);
@@ -20,3 +28,32 @@ app.listen(9090, err => {
     }
     console.log('Listening on: 9090');
 });
+
+/*app.post('/insert',function (req,res) {
+    console.log('successfully posted with express and axios');
+});*/
+
+
+app.post('/insert', function(req, res, next) {
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server: req.body: " + req.body);
+        var collection = db.collection('artwork');
+        // Insert some documents
+        collection.insert([
+            {
+                "title":req.body.title,
+                "artist": req.body.artist,
+                "available": true,
+                "blockchain_id": "blockchain_id",
+                "date_added": new Date(),
+                "description": req.body.desc,
+                "price": req.body.price,
+                "subject_type": "somesubj",
+                "image_url" : req.body.img
+            }], function(err, result) {
+        });
+        db.close();
+    });
+});
+
